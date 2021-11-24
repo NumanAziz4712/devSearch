2ccb7e1285c2efb2716657a076ad39dadb51c188
Numan Aziz <numanaziz4712@gmail.com>
2021-11-24 22:30:56 +0500

    added ordering method to the developers profiles

--- users/models.py
+++ users/models.py
@@ -26,8 +26,10 @@ class Profiles(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-
         return str(self.username)
+    
+    class Meta:
+        ordering = ['created']
 
 
 
